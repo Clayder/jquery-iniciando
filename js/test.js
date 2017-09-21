7d4ae917816e7c48350953d814c731a8640a0059
Peter Clayder <peterclayder@id.uff.br>
2017-09-21 20:24:20 -0300

    utilizei o append e prepend

--- js/test.js
+++ js/test.js
@@ -1,15 +1,24 @@
 $(document).ready(function(){
+    
+    // muda a cor do titulo
     $('#titulo').html('Jquery').css('color', 'red');
+    
+    // printa paragravo 
     $('#desc').html('paragrafo');
 
+    // modifica a cor do button ao clicar 
     $('#corButton').click(function(){
         $(this).css('color', '#00ff00');
     });
 
+    // esconde o paragrafo
     $('#escParagrafo').click(function(){
         $('#escParagrafo20').hide();
     });
 
+    /* ############### Ações do mouse ###################### */
+
+    // modifica o conteúdo do button
     $('#click').click(function(){
         $(this).html('OK');
     });
@@ -29,17 +38,48 @@ $(document).ready(function(){
         $(this).html('OK');
     });
 
+    /* ####################################################### */
+
+    /* ############### Ações do teclado ###################### */
     $('#keypress').keypress(function(){
         $("#eventKey").html('keypress -> ' + $(this).val());
     });
-
     $('#keydown').keydown(function(){
         $("#eventKey").html('keydown -> ' + $(this).val());
     });
-
     $('#keyup').keyup(function(){
         $("#eventKey").html('keyup -> ' + $(this).val());
     });
+    /* ######################################################## */
+
+
+    $("#removeClass").hide();
+    // adiciona class
+    $("#addClass").click(function(){
+        $(this).addClass("btn btn-info btn-large");
+        $("#removeClass").show();
+    });
+
+    // remove class
+    $("#removeClass").click(function(){
+        $("#addClass").removeClass("btn btn-info btn-large");
+        $("#removeClass").hide();
+    });
+
+    // inserir paragragro abaixo
+    var qtdPara = 0;
+    $("#addParagrafo").click(function(){
+        $("#paragrafo").append("<p id='para'"+qtdPara+"> paragrafo "+qtdPara+" </p>");
+        qtdPara++;
+    });
+
+    // inserir paragragro acima
+    $("#addParagrafoAc").click(function(){
+        $("#paragrafo").prepend("<p id='para'"+qtdPara+"> paragrafo "+qtdPara+" </p>");
+        qtdPara++;
+    });
+
+  
 
 
 });
