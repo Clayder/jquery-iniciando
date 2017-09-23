ab546d5f5c19c5157edb4ce9216a871b0653bc12
Peter Clayder <peterclayder@id.uff.br>
2017-09-22 21:20:50 -0300

    finalizei a validação do formulário

--- js/form.js
+++ js/form.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
-    alert();
+    $('form').submit(function(){
+       
+        var nome = $(':input[name=nome]').val();
+        var email = $(':input[name=email]').val();
+        var tel = $(':input[name=tel]').val();
+        // checkbox
+        var term = $(':input[name=term]').is(":checked");
+
+        var errors = "";
+        // limpa o elemento
+        $('#errors').html("");
+
+        if(nome === "" || email === "" || tel === "" || !term){
+            errors = "Todos os campos são obrigatórios";
+        }
+        if(errors){
+            $('#errors').html("<div class='alert alert-danger' role='alert'>"+  errors + "</div>");
+            return false;
+        }
+        return true;
+    });
 });
